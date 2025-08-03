96276c6e340b6aaf82df33ba6165ac405ce170de
Anton Olson <ols00559@morris.umn.edu>
2025-08-03 18:42:26 -0500

    I've begun updating the routing of my website, so these files will look a tad bit wonky for a little bit.

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from "./app/home/home.component";
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [HomeComponent],
 })
 export class AppComponent {
   title = 'Website';
--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './app/home/home.component';
+
 
 export const routes: Routes = [];
