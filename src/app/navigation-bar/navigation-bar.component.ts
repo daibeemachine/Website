f084e7f2fb413d9db7ff4217c43c4b66482b13e0
Anton Olson <ols00559@morris.umn.edu>
2025-08-12 18:52:24 -0500

    Added a close button and a new icon for the home menu (with some animations)! :D

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -10,16 +10,23 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
-  private menu = document.querySelector('.pages');
   private opened = false;
   public slideMenu()
   {
     var menu = document.querySelector('.pages');
+    var menuIcon = document.querySelector('.menu');
+    if(menuIcon == null) menuIcon = document.querySelector('.disabled_menu')
+    var close = document.querySelector('.close');
     this.opened = !this.opened;
-    menu?.setAttribute('style', 'transform: translateX(-75%); transition: transform 200ms');
+    menu?.setAttribute('style', 'transform: translateX(-75%) translateY(-50%); transition: transform 200ms');
+    close?.setAttribute('style', 'transform: translateY(0%); transition: transform 200ms');
+    menuIcon?.setAttribute('class', 'menu');
     if(this.opened)
     {
-      menu?.setAttribute('style', 'transform: translateX(0%); transition: transform 200ms');
+      menu?.setAttribute('style',
+        'transform: translateX(0%) translateY(-50%); transition: transform 200ms;');
+      close?.setAttribute('style', 'transform: translateY(100%);');
+      menuIcon?.setAttribute('class', 'disabled_menu')
     }
   }
 }
