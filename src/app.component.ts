cfc72a2d6e1ae405ecbb7d95831c0696b3c55f9b
Anton Olson <ols00559@morris.umn.edu>
2025-08-11 22:41:39 -0500

    Made the navigation bar (technically) functional! I added three temporary columns and added an effect for when you click the menu button.

--- src/app.component.ts
+++ src/app.component.ts
@@ -3,13 +3,12 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { PortalModule } from '@angular/cdk/portal';
 import { NavigationBarComponent } from './app/navigation-bar/navigation-bar.component';
-import { NavigationBarOptionsComponent } from "./app/navigation-bar-options/navigation-bar-options.component";
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterModule, NavigationBarComponent, CommonModule, PortalModule, NavigationBarOptionsComponent],
+  imports: [RouterModule, NavigationBarComponent, CommonModule, PortalModule],
 })
 export class AppComponent {
   title = 'Website';
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -10,5 +10,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
-
+  private menu = document.querySelector('.pages');
+  private opened = false;
+  public slideMenu()
+  {
+    var menu = document.querySelector('.pages');
+    this.opened = !this.opened;
+    menu?.setAttribute('style', 'transform: translateX(-75%); transition: transform 200ms');
+    if(this.opened)
+    {
+      menu?.setAttribute('style', 'transform: translateX(0%); transition: transform 200ms');
+    }
+  }
 }
