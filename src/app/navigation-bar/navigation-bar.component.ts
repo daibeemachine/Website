7900692c5cb8a398639dd10107364ab7c973980a
Anton Olson <ols00559@morris.umn.edu>
2025-08-13 17:22:58 -0500

    I added responsive scaling to my navigation menu in this commit!!! Also, I added responsive scaling to the home menu, too using CSS' clamp() function instead of @media.

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -10,23 +10,29 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
+
   private opened = false;
   public slideMenu()
   {
     var menu = document.querySelector('.pages');
+
     var menuIcon = document.querySelector('.menu');
-    if(menuIcon == null) menuIcon = document.querySelector('.disabled_menu')
-    var close = document.querySelector('.close');
+    if(menuIcon == null) menuIcon = document.querySelector('.disabled_menu');
+
+    var closeIcon = document.querySelector('.close');
+    if(closeIcon == null) closeIcon = document.querySelector('.close_opened');
+
     this.opened = !this.opened;
     menu?.setAttribute('style', 'transform: translateX(-75%) translateY(-50%); transition: transform 200ms');
-    close?.setAttribute('style', 'transform: translateY(0%); transition: transform 200ms');
     menuIcon?.setAttribute('class', 'menu');
+    closeIcon?.setAttribute('class', 'close');
+
     if(this.opened)
     {
       menu?.setAttribute('style',
         'transform: translateX(0%) translateY(-50%); transition: transform 200ms;');
-      close?.setAttribute('style', 'transform: translateY(100%);');
-      menuIcon?.setAttribute('class', 'disabled_menu')
+      closeIcon?.setAttribute('class', `close_opened`);
+      menuIcon?.setAttribute('class', 'disabled_menu');
     }
   }
 }
