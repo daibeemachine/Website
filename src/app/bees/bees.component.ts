7f14debc794d5c321949beaa45e3ce89999cb2db
daibeemachine <daibeemachine@gmail.com>
2026-07-18 19:05:57 -0500

    Set the secret bee page as a 404 page

--- src/app/bees/bees.component.ts
+++ src/app/bees/bees.component.ts
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './bees.component.scss'
 })
 export class BeesComponent {
+  public flipBee() {
+    var bee = document.querySelector('#flip');
+    bee?.setAttribute('style', 'transform: rotateX(360deg); transition: transform 500ms');
+    setTimeout(() => { bee?.setAttribute('style', ''); }, 500);
+  }
+
+  public spinBee() {
+    var bee = document.querySelector('#spin');
+    bee?.setAttribute('style', 'transform: rotate(-360deg); transition: transform 500ms');
+    setTimeout(() => { bee?.setAttribute('style', ''); }, 500);
+  }
+
+  public squishBee() {
+    var bee = document.querySelector('#squish');
+    bee?.setAttribute('style', 'transform: translateY(-100%); transition: transform 300ms ease-out');
+    setTimeout(() => { bee?.setAttribute('style', 'transform: translateY(0); transition: transform 300ms ease-in;'); }, 300);
+  }
 
 }
